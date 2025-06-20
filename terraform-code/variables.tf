07c1a216e78e69590f04c11ee038e6c16e7ecb57
preritrajK <prerit_rajkarnikar@outlook.com>
2025-06-20 20:28:11 +0000

    Create prod.tfvars, dev.auto.tfvars and prod.auto.tfvars

--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -3,3 +3,9 @@ variable "repo_count" {
     description = "Number of repositories."
     default = 1
 }
+
+variable "varsource" {
+  type = string
+  description = "src used to define variables" 
+  default = "variables.tf"
+}
