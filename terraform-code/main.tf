1dc3cb3c3c179b3b14db48bc38d946231732bc89
preritrajK <prerit_rajkarnikar@outlook.com>
2025-06-20 16:18:02 +0000

    add the count meta argument to match other resources and modify other identifiers

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -11,18 +11,20 @@ resource "github_repository" "terraform-repo" {
   auto_init   = true
 }
 
-# resource "github_repository_file" "readme" {
-#   repository          = github_repository.terraform-repo.name
-#   branch              = "main"
-#   file                = "Readme.md"
-#   content             = "# This repository is for Infra Devs"
-#   overwrite_on_create = true
-# }
+resource "github_repository_file" "readme" {
+  count = 2
+  repository          = github_repository.terraform-repo[count.index].name
+  branch              = "main"
+  file                = "Readme.md"
+  content             = "# This repository is for Infra Devs"
+  overwrite_on_create = true
+}
 
-# resource "github_repository_file" "index" {
-#   repository = github_repository.terraform-repo.name
-#   branch = "main"
-#   file = "index.html"
-#   content = "Hello Terraform"
-#   overwrite_on_create = "true"
-# } 
+resource "github_repository_file" "index" {
+  count = 2
+  repository = github_repository.terraform-repo[count.index].name
+  branch = "main"
+  file = "index.html"
+  content = "Hello Terraform"
+  overwrite_on_create = "true"
+} 
