352e86cdb6316e99054701e614926847e1cfd124
preritrajK <prerit_rajkarnikar@outlook.com>
2025-06-20 16:11:02 +0000

    Added random.id count index

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -1,26 +1,28 @@
 resource "random_id" "random" {
   byte_length = 2
+  count       = 2
 }
 
 resource "github_repository" "terraform-repo" {
-  name        = "terraform-repo-${random_id.random.dec}"
+  count       = 2
+  name        = "terraform-repo-${random_id.random[count.index].dec}"
   description = "Code for Terraform"
   visibility  = "public"
   auto_init   = true
 }
 
-resource "github_repository_file" "readme" {
-  repository          = github_repository.terraform-repo.name
-  branch              = "main"
-  file                = "Readme.md"
-  content             = "# This repository is for Infra Devs"
-  overwrite_on_create = true
-}
+# resource "github_repository_file" "readme" {
+#   repository          = github_repository.terraform-repo.name
+#   branch              = "main"
+#   file                = "Readme.md"
+#   content             = "# This repository is for Infra Devs"
+#   overwrite_on_create = true
+# }
 
-resource "github_repository_file" "index" {
-  repository = github_repository.terraform-repo.name
-  branch = "main"
-  file = "index.html"
-  content = "Hello Terraform"
-  overwrite_on_create = "true"
-} 
+# resource "github_repository_file" "index" {
+#   repository = github_repository.terraform-repo.name
+#   branch = "main"
+#   file = "index.html"
+#   content = "Hello Terraform"
+#   overwrite_on_create = "true"
+# } 
